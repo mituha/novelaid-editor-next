40d943193d284dd51ea177fe7ed0df90a7b0885f
Mituha Mizuki <mituha@restar.jp>
2026-03-20 22:53:14 +0900

    tauri-plugin-novelaid-fs追加

---
+++ tauri-plugin-novelaid-fs/src/lib.rs
@@ -0,0 +1,48 @@
+use tauri::{
+  plugin::{Builder, TauriPlugin},
+  Manager, Runtime,
+};
+
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
+mod error;
+mod models;
+
+pub use error::{Error, Result};
+
+#[cfg(desktop)]
+use desktop::NovelaidFs;
+#[cfg(mobile)]
+use mobile::NovelaidFs;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the novelaid-fs APIs.
+pub trait NovelaidFsExt<R: Runtime> {
+  fn novelaid_fs(&self) -> &NovelaidFs<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::NovelaidFsExt<R> for T {
+  fn novelaid_fs(&self) -> &NovelaidFs<R> {
+    self.state::<NovelaidFs<R>>().inner()
+  }
+}
+
+/// Initializes the plugin.
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("novelaid-fs")
+    .invoke_handler(tauri::generate_handler![commands::ping])
+    .setup(|app, api| {
+      #[cfg(mobile)]
+      let novelaid_fs = mobile::init(app, api)?;
+      #[cfg(desktop)]
+      let novelaid_fs = desktop::init(app, api)?;
+      app.manage(novelaid_fs);
+      Ok(())
+    })
+    .build()
+}
